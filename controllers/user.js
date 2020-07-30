3d6647ee755ea7111a1748ab3bd62c6e7581fd49
KrisiKostadinov <krisi.199898@gmail.com>
2020-07-30 17:47:28 +0300

    Added login logic.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     post: {
@@ -30,7 +31,24 @@ module.exports = {
         },
         
         async login(req, res) {
-            res.send('Hello!');
+            const { email, password } = req.body;
+
+            const user = await User.find({ email });
+
+            if(!user) {
+                return res.send({ error: 'The email or password is wrong!' });
+            }
+
+            jwt.sign({
+                user: user.email,
+                exp: Math.floor(Date.now() / 1000) + (60 * 60),
+            }, 'secret', (err, token) => {
+                if(err) {
+                    return res.send(err);
+                }
+
+                res.status(200).send({ token });
+            });
         }
     }
 }
